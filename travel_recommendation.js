b39adf2f308edb8347dd2a2a9e6b5dfa5d8bd5c8
thibautdegbetchi <thibautdegbetchi@gmail.com>
2024-09-07 03:13:22 -0400

    update travel-recommendation.js file

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,5 +1,24 @@
-let data;
+let datas;
 const url = './travel_recommendation_api.json';
 fetch(url).then((res)=>res.json()).then(function(data){
-    console.log(data);
+    datas = data;
+    console.log(datas);
 }).catch((err)=> console.log(err));
+
+let search;
+
+function toSearch(){
+    search=document.getElementById('search').value.toString().toLowerCase();
+
+    if(search==="city" || search==="cities" || search==="ville" || search==="villes"){
+        
+    }else if(search==="country" || search==="countries" || search==="pays"){
+
+    }else if(search==="beach" || search==="beaches" || search==="plage" || search==="plages" ){
+
+    }else if(search==="temple" || search==="temples"){
+
+    }else{
+
+    }
+}
